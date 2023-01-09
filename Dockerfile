b49ff69bc9c6457b378c02dcb568dbf655cda823
최성우/SRE팀 <seongwoo.choi@musinsa.com>
2023-01-09 23:52:15 +0900

    refactor

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,16 @@ FROM golang:1.19.3-buster AS build
 
 WORKDIR /app
 
-COPY go.mod ./
-COPY go.sum ./
+COPY . .
 
 RUN go mod download && go mod verify
 
 COPY main.go ./
 
-RUN go build -o /my-app
+RUN go build main.go
 
 FROM gcr.io/distroless/base-debian11
 
-COPY --from=build /my-app /my-app
+COPY --from=build /main /main
 
-ENTRYPOINT ["/my-app"]
+ENTRYPOINT ["/main"]
--- service/securityGroup/securityGroup.go
+++ service/securityGroup/securityGroup.go
@@ -14,44 +14,45 @@ import (
 func GetSecurityGroup(c *fiber.Ctx) error {
 	resourceType := c.Query("type", "")
 	resourceName := c.Query("name", "")
+
 	switch resourceType {
 	case "alb":
 		err, alb := alb.GetSecurityGroup(resourceName)
 		if err != nil {
-			return c.JSON(fiber.Map{"status": "404", "message": err.Error()})
+			return c.JSON(fiber.Map{"message": err.Error()})
 		}
 		return c.JSON(alb)
 	case "docdb":
 		err, docdb := docdb.GetSecurityGroup(resourceName)
 		if err != nil {
-			return c.JSON(fiber.Map{"status": "404", "message": err.Error()})
+			return c.JSON(fiber.Map{"message": err.Error()})
 		}
 		return c.JSON(docdb)
 	case "msk":
 		err, msk := msk.GetSecurityGroup(resourceName)
 		if err != nil {
-			return c.JSON(fiber.Map{"status": "404", "message": err.Error()})
+			return c.JSON(fiber.Map{"message": err.Error()})
 		}
 		return c.JSON(msk)
 	case "rds":
 		err, rds := rds.GetSecurityGroup(resourceName)
 		if err != nil {
-			return c.JSON(fiber.Map{"status": "404", "message": err.Error()})
+			return c.JSON(fiber.Map{"message": err.Error()})
 		}
 		return c.JSON(rds)
 	case "elasticache":
 		err, elasticache := elasticache.GetSecurityGroup(resourceName)
 		if err != nil {
-			return c.JSON(fiber.Map{"status": "404", "message": err.Error()})
+			return c.JSON(fiber.Map{"message": err.Error()})
 		}
 		return c.JSON(elasticache)
 	case "ec2":
 		err, ec2 := ec2.GetSecurityGroup(resourceName)
 		if err != nil {
-			return c.JSON(fiber.Map{"status": "404", "message": err.Error()})
+			return c.JSON(fiber.Map{"message": err.Error()})
 		}
 		return c.JSON(ec2)
+	default:
+		return c.JSON(fiber.Map{"status": "404", "message": "옳바른 리소스 타입을 입력해주세요(alb, elasticache, msk, rds, docdb, rds, ec2)"})
 	}
-
-	return c.JSON(fiber.Map{"status": "404", "message": "옳바른 리소스 타입을 입력해주세요(alb, elasticache, msk, rds, docdb, rds, ec2)"})
 }
